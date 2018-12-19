8fdbcd0191b4a63006d0c58a461c486b8cd79837
Avery Wagar <ajmw.subs@gmail.com>
2018-12-18 20:15:01 -0800

    Rust library - cli

---
+++ src/bin.rs
@@ -0,0 +1,34 @@
+
+extern crate primeval;
+
+fn main(){
+    use std::env;
+    // Constants
+    let version: String = "Primeval 0.0.2 Copyright 2018 Avery Wagar".to_string();
+    let help: String = "Primeval: A montrosity of a prime generator.\n\nCommands\n=========================\ngen <upper bound>: generate primes until upper bound\nprime <number>: returns if number is prime\nhelp: shows this help menu\nversion: shows version info\n\nContact\n======================\ngithub: https://github.com/ajmwagar/primeval-rs\nemail: ajmw.subs@gmail.com".to_string();
+
+    let args: Vec<String> = env::args().collect();
+    if &args.len() > &1 {
+
+        let sub = &args[1];
+        let sub_slice: &str = &sub[..];
+
+        match sub_slice {
+            "gen" => println!("{:?}",  primeval::primes_gen(args[2].parse::<usize>().unwrap().clone())),
+            "prime"=> println!("{}", primeval::is_prime(args[2].parse::<usize>().unwrap().clone(), &primeval::primes_gen(args[2].parse::<usize>().unwrap().clone()))),
+            "version" => println!("{}", version),
+            "help" => println!("{}", help),
+            _ => println!("Incorrect command. Try primeval help.")
+
+        }
+    }
+    else {
+        println!("No command issued. Try primeval help");
+    }
+
+}
+
+
+
+
+
--- src/primeval.rs
+++ src/primeval.rs
@@ -1,21 +1,23 @@
 #![feature(test)]
 extern crate test;
 
-use std::env;
-
-
-fn is_prime(n: usize, primes: &Vec<usize>) -> bool {
-    for &p in primes {
-        let q: usize = n / p as usize;
-        if q < p as usize { return true };
-        let r = n - q * p as usize;
-        if r == 0 { return false };
+pub fn is_prime(n: usize, primes: &Vec<usize>) -> bool {
+    if n > 1 {
+        for &p in primes {
+            let q: usize = n / p as usize;
+            if q < p as usize { return true };
+            let r = n - q * p as usize;
+            if r == 0 { return false };
+        }
+        panic!("too few primes")
     }
-    panic!("too few primes")
+    else {
+        return false;
+    }
 }
 
 // Use Sieve_of_Eratosthenes for prime generation (https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes)
-fn primes_gen(bound: usize) -> Vec<usize> {
+pub fn primes_gen(bound: usize) -> Vec<usize> {
     let mut primes: Vec<bool> = (0..bound + 1).map(|num| num == 2 || num & 1 != 0).collect();
     let mut num = 3usize;
     while num * num <= bound {        
@@ -28,34 +30,6 @@ fn primes_gen(bound: usize) -> Vec<usize> {
     }
     primes.into_iter().enumerate().skip(2).filter_map(|(i, p)| if p {Some(i)} else {None}).collect::<Vec<usize>>()
 }
-
-fn main(){
-    // Constants
-    let version: String = "Primeval 0.0.2 Copyright 2018 Avery Wagar".to_string();
-    let help: String = "Primeval: A montrosity of a prime generator.\n\nCommands\n=========================\ngen <upper bound>: generate primes until upper bound\nprime <number>: returns if number is prime\nhelp: shows this help menu\nversion: shows version info\n\nContact\n======================\ngithub: https://github.com/ajmwagar/primeval-rs\nemail: ajmw.subs@gmail.com".to_string();
-
-    let args: Vec<String> = env::args().collect();
-    if &args.len() > &1 {
-
-    let sub = &args[1];
-    let sub_slice: &str = &sub[..];
-
-    match sub_slice {
-        "gen" => println!("{:?}",  primes_gen(args[2].parse::<usize>().unwrap().clone())),
-        "prime"=> println!("{}", is_prime(args[2].parse::<usize>().unwrap().clone(), &primes_gen(args[2].parse::<usize>().unwrap().clone()))),
-        "version" => println!("{}", version),
-        "help" => println!("{}", help),
-        _ => println!("Incorrect command. Try primeval help.")
-
-    }
-    }
-    else {
-        println!("No command issued. Try primeval help");
-    }
-
-}
-
-
 
 #[cfg(test)]
 mod tests {
@@ -78,6 +52,15 @@ mod tests {
     fn is_two_prime_test(){
         assert_eq!(true, is_prime(2 as usize, &primes_gen(2 as usize)));
     }
+    #[test]
+    fn is_0_prime(){
+        assert_eq!(false, is_prime(0 as usize, &primes_gen(0 as usize)));
+    }
+    #[test]
+    fn is_1_prime(){
+        assert_eq!(false, is_prime(1 as usize, &primes_gen(1 as usize)));
+    }
+
 
     #[bench]
     fn bench_100_primes(b: &mut Bencher){
